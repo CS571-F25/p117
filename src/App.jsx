e0c9f412fa57cdd7b1ab59a2ba32eb626631a245
harshnar192 <narendrakuma@wisc.edu>
2025-11-05 15:57:34 -0600

    fixing routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { HashRouter, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes, Link } from 'react-router'
 import './App.css'
 import Home from './components/Home'
 import AboutMe from './components/AboutMe'
 
 function App() {
   return <HashRouter>
+    <nav>
+      <Link to="/">Home</Link> | <Link to="/about">About Me</Link>
+    </nav>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/about" element={<AboutMe/>}></Route>
